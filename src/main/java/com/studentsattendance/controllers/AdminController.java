3ede673756b5d0e19760cc5ffa77e89142787b39
abedi <abedila201@gmail.com>
2023-05-17 21:05:46 +0300

    الانتهاء ان شاء الله من صفحة الادمن(:

--- src/main/java/com/studentsattendance/controllers/AdminController.java
+++ src/main/java/com/studentsattendance/controllers/AdminController.java
@@ -51,9 +51,35 @@ public class AdminController implements Initializable {
     @FXML
     private Button button3;
     @FXML
-    private Button logout;
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
     @FXML
     private Button buttonCreate;
+
+    @FXML
+    private Button buttonCreate1;
+
+    @FXML
+    private Button buttonCreate3;
+
+    @FXML
+    private Button buttonCreate31;
+
+    @FXML
+    private Button buttonCreate311;
+
+    @FXML
+    private Button buttonCreate32;
+    @FXML
+    private Button logout;
+    @FXML
+    private Button buttonCreate11;
     @FXML
     private TextField textCourseBook;
 
@@ -81,6 +107,14 @@ public class AdminController implements Initializable {
 
     @FXML
     private AnchorPane page3;
+    @FXML
+    private AnchorPane page4;
+
+    @FXML
+    private AnchorPane page5;
+
+    @FXML
+    private AnchorPane page6;
 
     Navigation navigation = new Navigation();
 
@@ -113,17 +147,54 @@ public class AdminController implements Initializable {
             page1.setVisible(true);
             page2.setVisible(false);
             page3.setVisible(false);
+            page4.setVisible(false);
+            page5.setVisible(false);
+            page6.setVisible(false);
 
         } else if (event.getSource() == button2) {
             page1.setVisible(false);
             page2.setVisible(true);
             page3.setVisible(false);
+            page4.setVisible(false);
+            page5.setVisible(false);
+            page6.setVisible(false);
         } else if (event.getSource() == button3) {
             page1.setVisible(false);
             page2.setVisible(false);
             page3.setVisible(true);
+            page4.setVisible(false);
+            page5.setVisible(false);
+            page6.setVisible(false);
 
         }
+        else if (event.getSource() == button4) {
+            page1.setVisible(false);
+            page2.setVisible(false);
+            page3.setVisible(false);
+            page4.setVisible(true);
+            page5.setVisible(false);
+            page6.setVisible(false);
+
+        }
+        else if (event.getSource() == button5) {
+            page1.setVisible(false);
+            page2.setVisible(false);
+            page3.setVisible(false);
+            page4.setVisible(false);
+            page5.setVisible(true);
+            page6.setVisible(false);
+
+        }
+        else if (event.getSource() == button6) {
+            page1.setVisible(false);
+            page2.setVisible(false);
+            page3.setVisible(false);
+            page4.setVisible(false);
+            page5.setVisible(false);
+            page6.setVisible(true);
+
+        }
+
     }
 
     public void onCreateCourse(){
@@ -151,6 +222,24 @@ public class AdminController implements Initializable {
         });
         button3.setOnMouseExited(e -> {
             button3.setStyle("-fx-background-color: #D3D3D3; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
+        });
+        button4.setOnMouseEntered(e -> {
+            button4.setStyle("-fx-background-color: #89cfff; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
+        });
+        button4.setOnMouseExited(e -> {
+            button4.setStyle("-fx-background-color: #D3D3D3; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
+        });
+        button5.setOnMouseEntered(e -> {
+            button5.setStyle("-fx-background-color: #89cfff; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
+        });
+        button5.setOnMouseExited(e -> {
+            button5.setStyle("-fx-background-color: #D3D3D3; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
+        });
+        button6.setOnMouseEntered(e -> {
+            button6.setStyle("-fx-background-color: #89cfff; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
+        });
+        button6.setOnMouseExited(e -> {
+            button6.setStyle("-fx-background-color: #D3D3D3; -fx-border-color:  #808080; -fx-border-width:  0px 0px 2px 0px;");
         });
         logout.setOnMouseEntered(e -> {
             logout.setStyle("-fx-background-color: #4997D0;");
